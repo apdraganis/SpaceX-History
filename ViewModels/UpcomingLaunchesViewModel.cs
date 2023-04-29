67ccc5ee7781f38c81ad1ff155cc577f83460534
tolis <draganisdev@gmail.com>
2023-04-29 19:46:52 +0300

    create UpcomingLaunchesViewModel, start building view code-behind, and view

--- ViewModels/UpcomingLaunchesViewModel.cs
+++ ViewModels/UpcomingLaunchesViewModel.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using SpaceXHistory.Helpers;
+using SpaceXHistory.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpaceXHistory.ViewModels
 {
-    internal class UpcomingLaunchesViewModel
+    public class UpcomingLaunchesViewModel : BaseViewModel
     {
+        public ObservableCollection<Root> NextLaunch {  get; set; }
+
+        private readonly HttpClient _httpClient;
+
+        public UpcomingLaunchesViewModel()
+        {
+            _httpClient = new HttpClient();
+            NextLaunch = new ObservableCollection<Root>();
+        }
+
+        public void PopulateNextLaunch()
+        {
+            foreach (Root launch in FetchAllNextLaunches())
+            {
+                NextLaunch.Add(launch);
+            }
+        }
+
+        private ObservableCollection<Root> FetchAllNextLaunches()
+        {
+            var nextLaunchSerialized = _httpClient.GetStringAsync(Constants.BaseUrl + "launches/upcoming").Result;
+            var nextLaunchDeserialized = JsonConvert.DeserializeObject<List<Root>>(nextLaunchSerialized);
+
+            ObservableCollection<Root> allLaunches = new(nextLaunchDeserialized);
+
+            return allLaunches;
+        }
     }
 }
--- Views/UpcomingLaunchesPage.xaml.cs
+++ Views/UpcomingLaunchesPage.xaml.cs
@@ -1,9 +1,18 @@
+using SpaceXHistory.ViewModels;
+
 namespace SpaceXHistory.Views;
 
 public partial class UpcomingLaunchesPage : ContentPage
 {
+	private readonly UpcomingLaunchesViewModel _viewModel;
+
 	public UpcomingLaunchesPage()
 	{
 		InitializeComponent();
+
+		_viewModel = new UpcomingLaunchesViewModel();
+		BindingContext = _viewModel;
+
+		_viewModel.PopulateNextLaunch();
 	}
 }
